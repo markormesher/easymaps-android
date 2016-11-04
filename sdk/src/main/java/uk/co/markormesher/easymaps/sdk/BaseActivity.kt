f739e4efe4aa7b4e5da09cadc7e2d428c6ee3c81
Mark Ormesher <me@markormesher.co.uk>
2016-11-04 21:20:43 +0000

    Update versioning

--- sdk/src/main/java/uk/co/markormesher/easymaps/sdk/BaseActivity.kt
+++ sdk/src/main/java/uk/co/markormesher/easymaps/sdk/BaseActivity.kt
@@ -3,14 +3,17 @@ package uk.co.markormesher.easymaps.sdk
 import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import uk.co.chrisjenx.calligraphy.R
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig
 import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper
+import uk.co.chrisjenx.calligraphy.R
 
 open class BaseActivity : AppCompatActivity() {
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
+
+		// note: the actual fonts are in the app projects, not the SDK one, because
+		// assets can't be access from outside the SDK.
 		CalligraphyConfig.initDefault(CalligraphyConfig.Builder()
 				.setDefaultFontPath("fonts/Lato-Regular.ttf")
 				.setFontAttrId(R.attr.fontPath)
